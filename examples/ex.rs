44c51030755145cabe25164cba4f0e18ada7480d
vrindisbacher <vrindisbacher77@gmail.com>
2025-08-04 11:47:27 -0700

    clean up

--- examples/ex.rs
+++ examples/ex.rs
@@ -15,23 +15,35 @@ struct Testing {
     age: u32,
 }
 
-fn main() {
+const FILE_PATH: &'static str = "testing_data.json";
+
+fn create_file() {
     let my_testing = Testing {
         name: "Tommy".to_string(),
         age: 32,
     };
     let as_string = serde_json::to_string(&my_testing).unwrap();
     // Write to file
-    std::fs::write("testing_data.json", &as_string).unwrap();
+    std::fs::write(FILE_PATH, &as_string).unwrap();
+}
 
-    // Read back from file
-    let file_contents = std::fs::read_to_string("testing_data.json").unwrap();
-    println!("File contents: {}", file_contents);
+fn read_file() -> String {
+    std::fs::read_to_string(FILE_PATH).unwrap()
+}
 
+fn main() {
+    // create ex file
+    create_file();
+
+    // read contents of ex file
+    let file_contents = read_file();
+
+    // parse into a privileged accessor - can access both fields
     let privileged: TestingPrivilegedAccessor = serde_json::from_str(&file_contents).unwrap();
     println!("{}", privileged.name());
     println!("{}", privileged.age());
 
+    // parse into a UNprivileged accessor - can access only name()
     let unprivileged: TestingUnprivilegedAccessor = serde_json::from_str(&file_contents).unwrap();
     println!("{}", unprivileged.name());
     // EX: The types will prevent this since `age` is sensitive
